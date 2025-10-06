5ddea1dc261554b1e6f8151df00945254c96b005
mate-khucishvili <materobloxs555@gmail.com>
2025-10-06 22:04:45 +0400

    second commit

---
+++ group 63/Level 68/classwork/Task-1/classwork.py
@@ -0,0 +1,11 @@
+# position: static;
+#  ეს არის default მნიშვნელობა
+#  ელემენტი იკავებს თავის ადგილს დოკუმენტის სტანდარტულად
+#  top left right bottom მასზე არ მოქმედებს
+
+# position: relative;
+# ელემენტი რჩება დოკუმენტში სტანდარტულად
+# მაგრამ შეგვიძლია მისი გადაადგილება top left right bottom დახმარებით
+
+# position: absolute;
+# top left right bottom მასზე მოქმედებს პირდაპირ.
